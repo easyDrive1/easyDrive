c8b10ddad40d88ef32ab02aff68c71eca2b59fd5
amooor94 <amooor@dhcp-075216.eduroam.chalmers.se>
2015-03-02 16:30:50 +0100

    hello
    hello

---
+++ app/src/main/java/com/example/amooor/test/About.java
@@ -0,0 +1,20 @@
+package com.example.amooor.test;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by amooor on 15-03-02.
+ */
+public class About extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.lay_about, container, false);
+        return rootView;
+    }
+}
